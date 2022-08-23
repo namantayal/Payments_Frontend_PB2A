d109d06f5e32c6184d0efcd2fbd9543d1be8d267
Anshika-Mohan <71456647+Anshika-Mohan@users.noreply.github.com>
2022-08-23 17:59:01 +0530

    dashboard files

--- src/components/DeleteRegisteredBiller.js
+++ src/components/DeleteRegisteredBiller.js
@@ -1,13 +1,35 @@
 
-import React from 'react';
+import React,{useState} from 'react';
+import Form from "react-bootstrap/Form";
+import "./DeleteRegisteredBiller.css";
 
 export default function DeleteRegisteredBiller() {
-
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
   return(
     <div className='MenuBar'>
         <h2 className='button'>Delete Registered Biller</h2>
-        <button className='button'>Insert</button>
         
+        <Form className="form" >
+        <Form.Group size="lg" controlId="email">
+          <Form.Label className="labelEmail">Email</Form.Label>
+          <Form.Control
+            autoFocus
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+
+        </Form.Group>
+        <Form.Group size="lg" controlId="password">
+          <Form.Label className="label">Password</Form.Label>
+          <Form.Control
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </Form.Group>
+        </Form>
     </div>
 
   );
--- src/components/ParticularBiller.js
+++ src/components/ParticularBiller.js
@@ -1,12 +1,36 @@
-import React from 'react';
+import React,{useState} from 'react';
+import Form from "react-bootstrap/Form";
+import "./ParticularBiller.css";
 
 export default function ParticularBiller() {
 
+    const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return(
     <div className='MenuBar'>
-        <h2 className='button'>Particular Biller</h2>
-        <button className='button'>Insert</button>
+        <h2 className='button'>View payments done for particular biller</h2>
         
+        <Form className="form" >
+        <Form.Group size="lg" controlId="email">
+          <Form.Label className="labelEmail">Email</Form.Label>
+          <Form.Control
+            autoFocus
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+
+        </Form.Group>
+        <Form.Group size="lg" controlId="password">
+          <Form.Label className="label">Password</Form.Label>
+          <Form.Control
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </Form.Group>
+        </Form>
     </div>
 
   );
