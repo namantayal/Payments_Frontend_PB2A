e573e12c0b648e0c80691da843e5747ef3d2174b
Anshika-Mohan <71456647+Anshika-Mohan@users.noreply.github.com>
2022-08-23 10:22:46 +0530

    routes

--- src/Route.js
+++ src/Route.js
@@ -1,7 +1,11 @@
 import Login from "/Login/login";
+import DashboardUser from "./Dashboard/DashboardUser";
+import DashboardManager from "./Dashboard/DashboardManager"
 import { Route } from 'react-router';
 <Route exact path="/login">
 
   <Login />
+  <DashboardUser/>
+  <DashboardManager/>
 
 </Route>
