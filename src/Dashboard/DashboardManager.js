36b60b5d5462262de6dfd1041339647cb4a9e1d1
Anshika-Mohan <71456647+Anshika-Mohan@users.noreply.github.com>
2022-08-23 15:07:40 +0530

    css changes

--- src/Dashboard/DashboardManager.js
+++ src/Dashboard/DashboardManager.js
@@ -5,14 +5,9 @@ export default function DashboardManager() {
 
   return(
     <div className='MenuBar'>
-        <button className='button'>Dashboard</button>
-        <button className='button'>Delete Registered Biller</button>
-        <button className='button'>View Registered Biller</button>
-        <button className='button'>Add New Biller</button>
-        <button className='button'>View Scheduled Payments</button>
-        <button className='button'>Manual Payments</button>
-        <button className='button'>View Payments done for a payment biller </button>
-        <button className='button'> export file to csv</button>
+        <h2 className='button'>Dashboard</h2>
+        <button className='button'>Create new bill</button>
+        
     </div>
 
   );
--- src/Dashboard/DashboardUser.js
+++ src/Dashboard/DashboardUser.js
@@ -4,7 +4,16 @@ export default function DashboardUser() {
 
   return(
 
-    <h2>Dashboard</h2>
+    <div className='MenuBar'>
+    <h2 className='button'>Dashboard</h2>
+    <button className='button'>Delete Registered Biller</button>
+    <button className='button'>View Registered Biller</button>
+    <button className='button'>Add New Biller</button>
+    <button className='button'>View Scheduled Payments</button>
+    <button className='button'>Manual Payments</button>
+    <button className='button'>View Payments done for a payment biller </button>
+    <button className='button'> export file to csv</button>
+</div>
 
   );
 
